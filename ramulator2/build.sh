1465312a42f5b7111b61de053981533761f11f4f
TtitiO <ting_lin@sjtu.edu.cn>
2026-06-19 11:06:33 +0800

    Restore ramulator2 as v2.1 submodule

--- ramulator2/build.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build
-cd build
-cmake -DPython_EXECUTABLE=../.venv/bin/python ..
-make -j$(nproc)
-cd ..
-uv pip install --no-build-isolation -e .
-PYTHONPATH=python python -m ramulator codegen
--- ramulator2/src/ramulator/dram/lambdas.h
+++
@@ -1,6 +0,0 @@
-#ifndef RAMULATOR_DRAM_LAMBDAS_H
-#define RAMULATOR_DRAM_LAMBDAS_H
-
-#include "ramulator/dram/lambdas/power.h"
-
-#endif  // RAMULATOR_DRAM_LAMBDAS_H
